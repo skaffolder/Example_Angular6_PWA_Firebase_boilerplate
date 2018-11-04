9fb0bf3fe14f87a33fca5d719c9fd802f1012d8e
Luca Carducci <luca.carducci91@gmail.com>
2018-11-04 11:42:59 +0200

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCoqpzecVjPhWX5dFy1y17Lsz3O5UvDx_c',
+    authDomain: 'test-2-72e7b.firebaseapp.com',
+    databaseURL: 'https://test-2-72e7b.firebaseio.com',
+    projectId: 'test-2-72e7b',
+    storageBucket: 'test-2-72e7b.appspot.com',
+    messagingSenderId: '1004501184827'
+  }
+};
